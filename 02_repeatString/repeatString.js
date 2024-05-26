e792b22d4ddb40c9d97d44454dab8f3be9d76d0f
hilnik <166646223+hilnik@users.noreply.github.com>
2024-05-26 12:31:51 +0100

    completed task 2 correctly
    added the correct function and arguments to complete this correctly

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let result = [];
+if (num<0) {return 'ERROR'} else{
+for (let x = 1; x <= num; x++){
+    result += string;
+}}
+return(result.toString());
 };
 
 // Do not edit below this line
