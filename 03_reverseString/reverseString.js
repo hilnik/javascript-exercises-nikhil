e1f1fa0effe5eb5d641d6559c39023f804f91564
hilnik <166646223+hilnik@users.noreply.github.com>
2024-05-27 17:25:43 +0100

    completed task 3
    i did task three correctly, had to use split, reverse, join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+wordArray = word.split('');
+reversedArray = wordArray.reverse();
+reversedWord = reversedArray.join('');
+return reversedWord;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
