cbdf3c719fe01ea276826f6465ab2193275d0132
hilnik <166646223+hilnik@users.noreply.github.com>
2024-05-28 17:41:08 +0100

    did section 4 correctly, although with help

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(array, ...value) {
+     return result = array.filter(x => !value.includes(x))
+
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
