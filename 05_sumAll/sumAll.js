3d35b7e8d0a66e147835258e31895dde70184833
hilnik <166646223+hilnik@users.noreply.github.com>
2024-05-29 17:37:23 +0100

    finished task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
 
+const sumAll = function(num1, num2) {
+    sum = 0; 
+    if (typeof(num1)== typeof(1) && typeof(num2)== typeof(1) && num1 >= 0 && num2 >= 0 ){
+        if(num1<num2){
+            do{
+                sum += num1;
+                num1++
+            } while (num1 != num2+1)
+        }else{
+            do{
+                sum += num2;
+                ++num2
+            } while (num2 != num1+1)  
+        }
+    } else {
+    return "ERROR"}
+return sum;
 };
+
+console.log(sumAll(1,4))
 
 // Do not edit below this line
 module.exports = sumAll;
