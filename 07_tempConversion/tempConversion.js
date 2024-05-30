3cfad6a76f5c545711b2e9a60f7beddb7fb8a3b1
hilnik <166646223+hilnik@users.noreply.github.com>
2024-05-30 17:41:46 +0100

    finished all 7 tasks needed so far

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let fiveninths = Number(5/9);
+  let minus32 = Number(x) - 32;
+  let converted = minus32 * fiveninths;
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  // Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+  let ninefifths = Number(y * 9/5);
+  let converted = 32 + ninefifths;
+  return Math.round(converted * 10) / 10;
+
 };
 
 // Do not edit below this line
